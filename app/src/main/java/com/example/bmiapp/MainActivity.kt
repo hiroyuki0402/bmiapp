e4f01bf3086a914529719a662e04c2470442ff1f
hiroyuki0402 <hiro1990.cloud@gmail.comcd>
2023-04-24 21:04:48 +0900

    準備

--- app/src/main/java/com/example/bmiapp/MainActivity.kt
+++ app/src/main/java/com/example/bmiapp/MainActivity.kt
@@ -3,14 +3,20 @@ package com.example.bmiapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.bmiapp.ui.theme.BMIAppTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.material.Text
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,22 +28,34 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Column(
+                        horizontalAlignment = Alignment.Start,
+                        modifier = Modifier.padding(20.dp)
+                    ) {
+
+                        /// タイトル
+                        Text(
+                            text = "BMI計算",
+                            fontSize = 26.sp,
+                            fontWeight = FontWeight.Bold
+                        )
+
+                        Spacer(
+                            modifier = Modifier.height(20.dp)
+                        )
+
+                        /// 身長タイトル
+                        Text(
+                            text = "身長",
+                            fontWeight = FontWeight.Bold,
+                            color = Color.Blue
+                        )
+
+
+                    }
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    BMIAppTheme {
-        Greeting("Android")
-    }
 }
